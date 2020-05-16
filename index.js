98c4f94ef1451bb812377720a412c0249b6a8bd5
Oscar Medrano Castillo <omedranoc@hotmail.com>
2020-05-16 09:39:56 -0500

    I changed the file

--- index.js
+++ index.js
@@ -1,39 +1,38 @@
-class User1 {
- constructor (name, email) {
-    this.name = name,
-    this.email = email
+// First code challenge
 
- }
- data (){
+/* var mMass = 90;
+var mHeight = 1.80;
 
-  return
-     `
-    Name : ${this.name}
-    Email : ${this.email}
-    `
-     }
+var jMass = 80 ;  
+var jHeight = 1.70 ;
 
-}
+var mBMI =  mMass/(mHeight*mHeight);
 
-class User2 extends User1 {
- constructor(name, email,id){
-   super(name, email){
+var jBMI =  jMass/(jHeight*jHeight);
 
-     this.id = id
-   }
- }
- data (){
+var markVSjohn = mBMI > jBMI;
 
-  return
-     `
-    Name : ${this.name}
-    Email : ${this.email}
-    ID : ${this.id}
-    `
-     }
+console.log('is Marks BMI higher than Johns BMI?'+' '+ markVSjohn ) */
 
-}
+// Second code challenge 
+
+var johnAverangeScore = (150 + 120 + 103)/3;  //104
+
+var mikeAverangeScore = (116 +  94 + 123)/3;  //111
+
+var maryAverangeScore = (116 + 94 + 123)/3;   // 112
+
+if (johnAverangeScore > mikeAverangeScore &&  johnAverangeScore > maryAverangeScore ){
+        console.log("John wins")
+    } else if (
+    mikeAverangeScore > johnAverangeScore &&   mikeAverangeScore > maryAverangeScore){
+        console.log("Mike wins")
+    }else if(
+    maryAverangeScore > johnAverangeScore && maryAverangeScore > mikeAverangeScore  ){
+        console.log("Mary wins")
+    } else {
+        "there is a draw"
+    }
 
 
-const oscar = new User1 ("oscar", "omedranoc@hotmail.com");
-document.write(oscar.data());
+console.log(johnAverangeScore + ' ' + ' ' + mikeAverangeScore + ' ' + maryAverangeScore )
