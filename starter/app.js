8626cc3e43c50b52044166870ad76768748dc718
Oscar Medrano Castillo <omedranoc@hotmail.com>
2020-05-21 10:32:46 -0500

    I added the full  dice project

--- starter/app.js
+++ starter/app.js
@@ -9,30 +9,46 @@ GAME RULES:
 
 */
 
- var scores ; var roundScore ; var activePlayer ; 
+ var scores ; var roundScore ; var activePlayer ; var gamePlaying ;
 
- activePlayer = 0;
- scores = [0,0];
- roundScore = 0 ; 
-
-
- document.querySelector(".dice").style.display = "none";
-
- document.querySelector("#score-0").textContent = 0;
- document.querySelector("#score-1").textContent = 0;
- document.querySelector("#current-0").textContent = 0;
- document.querySelector("#current-1").textContent = 0;
+init();
 
  document.querySelector(".btn-roll").addEventListener("click", function (){
-     var dice = Math.floor(Math.random() * 6) + 1;
-     document.querySelector(".dice").style.display = "inline";
-     document.querySelector(".dice").src = "dice-"+ dice + ".png"
-     
-     if (dice !== 1){
-         roundScore = roundScore + dice;
-         document.querySelector("#current-" + activePlayer ).textContent = roundScore;
-     } else {
-         activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+     if (gamePlaying){
+        var dice = Math.floor(Math.random() * 6) + 1;
+        document.querySelector(".dice").style.display = "inline";
+        document.querySelector(".dice").src = "dice-"+ dice + ".png"
+        
+        if (dice !== 1){
+            roundScore = roundScore + dice;
+            document.querySelector("#current-" + activePlayer ).textContent = roundScore;
+        } else {
+            nextPlayer();
+        }
+     }
+    
+ })
+ 
+
+document.querySelector(".btn-hold").addEventListener("click", function (){
+    if (gamePlaying){
+        scores[activePlayer] = scores[activePlayer] + roundScore;
+        document.querySelector("#score-"+ activePlayer).textContent = scores[activePlayer];
+    
+        if(scores[activePlayer] >= 100){
+            document.querySelector("#name-"+ activePlayer).textContent = "Winner!";
+            document.querySelector(".player-"+activePlayer+"-panel").classList.add("winner");
+            document.querySelector(".player-"+activePlayer+"-panel").classList.remove("active");
+            gamePlaying = false ;
+        } else {
+            nextPlayer();
+        }
+    }
+    
+})
+ 
+function nextPlayer (){
+    activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
          roundScore = 0;
          document.querySelector("#current-"+ activePlayer).textContent = roundScore;  
 
@@ -43,9 +59,29 @@ GAME RULES:
          document.querySelector(".player-1-panel").classList.toggle("active");
 
          document.querySelector(".dice").style.display = "none";
-     }
- })
- 
- //document.querySelector("#score-"+ activePlayer).textContent = dice;
+};
 
- 
+document.querySelector(".btn-new").addEventListener("click", init);
+
+function init (){
+    activePlayer = 0;
+    scores = [0,0];
+    roundScore = 0 ; 
+    gamePlaying = true ;
+   
+    document.querySelector(".dice").style.display = "none";
+   
+    document.querySelector("#score-0").textContent = 0;
+    document.querySelector("#score-1").textContent = 0;
+    document.querySelector("#current-0").textContent = 0;
+    document.querySelector("#current-1").textContent = 0;
+    document.querySelector("#name-0").textContent = "saul" ;
+    document.querySelector("#name-1").textContent = "oscar";
+    document.querySelector(".player-0-panel").classList.add("active");
+    document.querySelector(".player-1-panel").classList.remove("active");
+    document.querySelector(".player-0-panel").classList.remove("winner");
+    document.querySelector(".player-1-panel").classList.remove("winner");
+    
+
+
+}
