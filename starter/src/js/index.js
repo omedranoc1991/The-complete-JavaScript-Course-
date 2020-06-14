5d127dde3aa79aa93215a02b9a9ea64aef5d8eb4
Oscar Medrano Castillo <omedranoc@hotmail.com>
2020-06-13 19:34:34 -0500

    I added the second part of the last project

--- starter/src/js/index.js
+++ starter/src/js/index.js
@@ -1,4 +1,8 @@
 import Search from "./models/Search"
+import {elements, renderLoader,clearLoader} from "./views/base"
+import * as searchView from "./views/searchViews"
+import Recipe from "./models/Recipe"
+import recipe from "./models/Recipe"
 
 //2.state:
 /*Global state of the app
@@ -9,25 +13,75 @@ import Search from "./models/Search"
 */
 const state = {}
 
-//4. controller
+//4. search controller
 const controlSearch = async () => {
 
     //a. get query from view
-    const query ="pizza"
+    const query = searchView.getInput()
+    
     if(query){
     //b. new search object and add to state 
     state.search = new Search(query)
     //c.prepare ui for results 
-    //d. search for recipes 
-    await state.search.getResults()
-    //e. render reesults on ui
-    console.log(state.search.result)
+    searchView.clearInput()
+    searchView.clearResults()
+    renderLoader(elements.searchRes)
+    try{
+   //d. search for recipes 
+   await state.search.getResults()
+   //e. render reesults on ui
+   clearLoader()
+   searchView.renderResults(state.search.result)
+    }catch(err){
+        alert("Something went wrong")
+    }
+ 
     }
 }
 
+//10. recipe controller
+const controlRecipe = async () =>{
+
+    //get id from url
+    const id = window.location.hash.replace("#","")
+    
+    if(id){
+        //prepare UI for changes 
+        //create a new recipe object
+        state.recipe = new Recipe(id)
+      
+        try{
+            //get recipe data
+            await state.recipe.getRecipe()
+            //calculate servings and time
+            state.recipe.calcTime()
+            state.recipe.calcServings()
+           
+            //render the recipe
+            console.log(state.recipe)
+        }catch(err){
+            alert("Something went wrong")
+        }
+        }
+       
+}
+
+window.addEventListener("hashchange", controlRecipe)
+window.addEventListener("load", controlRecipe)
+
 //3. add event listener "submit", is used to valid data
-document.querySelector(".search").addEventListener("submit", e => {
+elements.searchForm.addEventListener("submit", e => {
     e.preventDefault()
     controlSearch()
 })
 
+elements.searchResPages.addEventListener("click", e => {
+    const btn = e.target.closest(".btn-inline")
+    if (btn){
+        const goToPage = parseInt(btn.dataset.goto, 10)
+        searchView.clearResults()
+        searchView.renderResults(state.search.result, goToPage)
+        
+    }
+    
+})
