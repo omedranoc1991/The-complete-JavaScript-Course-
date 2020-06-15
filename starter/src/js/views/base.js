47409fe0b8d4475d8cc2d1fd88f175c4b7afad86
Oscar Medrano Castillo <omedranoc@hotmail.com>
2020-06-15 16:40:52 -0500

    I added the last part of the last project

--- starter/src/js/views/base.js
+++ starter/src/js/views/base.js
@@ -1,30 +1,31 @@
-//5. all the elements that we need from our app 
-
 export const elements = {
-    searchForm : document.querySelector(".search"),
-    searchInput: document.querySelector(".search__field"),
-    searchRes: document.querySelector(".results"),
-    searchResList : document.querySelector(".results__list"),
-    searchResPages : document.querySelector(".results__pages")
-}
+    searchForm: document.querySelector('.search'),
+    searchInput: document.querySelector('.search__field'),
+    searchRes: document.querySelector('.results'),
+    searchResList: document.querySelector('.results__list'),
+    searchResPages: document.querySelector('.results__pages'),
+    recipe: document.querySelector('.recipe'),
+    shopping: document.querySelector('.shopping__list'),
+    likesMenu: document.querySelector('.likes__field'),
+    likesList: document.querySelector('.likes__list')
+};
 
-//8. create a loader animation
-export const elementStrings ={
-    loader : 'loader'
-}
+export const elementStrings = {
+    loader: 'loader'
+};
 
 export const renderLoader = parent => {
-    const loader =`
-       <div class = "${elementStrings.loader}">
-           <svg>
-               <use href= "img/icons.svg#icon-cw"></use>
-           </svg>
-       </div>
+    const loader = `
+        <div class="${elementStrings.loader}">
+            <svg>
+                <use href="img/icons.svg#icon-cw"></use>
+            </svg>
+        </div>
     `;
-    parent.insertAdjacentHTML("afterbegin",loader)
-}
+    parent.insertAdjacentHTML('afterbegin', loader);
+};
 
 export const clearLoader = () => {
-    const loader = document.querySelector(`.${elementStrings.loader}`)
-    if(loader) loader.parentElement.removeChild(loader)
-}
+    const loader = document.querySelector(`.${elementStrings.loader}`);
+    if (loader) loader.parentElement.removeChild(loader);
+};
--- starter/src/js/views/searchViews.js
+++ starter/src/js/views/searchViews.js
@@ -16,6 +16,14 @@ export const clearResults = () =>{
     elements.searchResPages.innerHTML =''
 }
 
+export const highlightSelect = id =>{
+    const resultsArr = Array.from(document.querySelectorAll('.results__link'))
+    resultsArr.forEach(el => {
+        el.classList.remove('results__link--active')
+    })
+    document.querySelector(`a[href*="${id}"]`).classList.add('results__link--active')
+}
+
 //7. Reduce string to ..
 const limitRecipeTitle = (title, limit = 17 ) =>{
     const newTitle = [];
